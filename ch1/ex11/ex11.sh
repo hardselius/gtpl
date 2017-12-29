baf2109dfbe13be893b856dd6f5fe575beaeffa9
Martin Hardselius <martin.hardselius@gmail.com>
2017-12-29 02:00:29 +0100

    Add solution to exercise 1.11

---
+++ ch1/ex11/ex11.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+go run ../ex10/ex10.go http://google.com http://facebook.com http://youtube.com http://baidu.com http://yahoo.com http://amazon.com http://qq.com http://twitter.com http://taobao.com http://live.com http://google.co.in
